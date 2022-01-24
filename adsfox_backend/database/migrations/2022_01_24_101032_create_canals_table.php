8c274cdef13aab089d366dc0bd33feaa3549023e
Eryk1997 <erykmati@o2.pl>
2022-01-24 11:14:40 +0100

    Update 2022_01_24_101032_create_canals_table.php
    update migrations

--- adsfox_backend/database/migrations/2022_01_24_101032_create_canals_table.php
+++ adsfox_backend/database/migrations/2022_01_24_101032_create_canals_table.php
@@ -15,6 +15,8 @@ class CreateCanalsTable extends Migration
     {
         Schema::create('canals', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('number');
             $table->timestamps();
         });
     }
