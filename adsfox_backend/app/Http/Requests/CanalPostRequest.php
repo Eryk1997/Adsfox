1eb864551e4ac2e9728d52100ad50ad8c24cf00a
Eryk1997 <erykmati@o2.pl>
2022-01-24 12:35:11 +0100

    Create CanalPostRequest.php
    create validations to post method for canals

---
+++ adsfox_backend/app/Http/Requests/CanalPostRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CanalPostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            // add validations to request 
+            'name'=>'required|unique:canals|between:2,100',
+            'number'=>'required|numeric|min:1'
+        ];
+    }
+}
