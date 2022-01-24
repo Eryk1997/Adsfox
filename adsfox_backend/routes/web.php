a343aba3d4723b18177fbfc725d4a9b7a2a1327e
Eryk1997 <erykmati@o2.pl>
2022-01-24 14:23:26 +0100

    Update web.php
    remove

--- adsfox_backend/routes/web.php
+++ adsfox_backend/routes/web.php
@@ -18,14 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/canals/percentages', [CanalController::class, 'percentage']);
 
-// options 1 to routing
-Route::get('/canals', [CanalController::class, 'index']);
-Route::post('/canals', [CanalController::class, 'store']);
-Route::get('canals/{id}', [CanalController::class, 'show']);
-Route::put('canals/{id}', [CanalController::class, 'update']);
-Route::delete('canals/{id}', [CanalController::class, 'destroy']);
-
-// options 2 to routing
-//Route::resource('/canals',CanalController::class);
