5f2b150104ac6feecf4bc0f5f35629e2661ef6ea
Eryk1997 <erykmati@o2.pl>
2022-01-25 10:02:52 +0100

    Update ListCanal.js
    update view

--- adsfox_frontend/src/views/listCanal/ListCanal.js
+++ adsfox_frontend/src/views/listCanal/ListCanal.js
@@ -1,14 +1,12 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-
 import "./style.css";
-
 import CanalService from "../../services/CanalService";
-
 import { GlobalContext } from "../../context/GlobalState";
 
 const ListCanal = () => {
-  const { canals,state, dispatch, setCanals,setPercentages } = useContext(GlobalContext);
+  const { canals, state, setCanals, setPercentages } =
+    useContext(GlobalContext);
 
   useEffect(async () => {
     retriveCanals();
@@ -32,46 +30,43 @@ const ListCanal = () => {
   };
 
   return (
-    <div className="col-10 d-flex justify-content-center m-auto mt-5">
+    <div>
       {canals.length > 0 ? (
-      <table class="table table-striped">
-        <thead className="">
-          <tr>
-            <th scope="col">Kanał</th>
-            <th scope="col">Ilość</th>
-            <td>
-            <Link
-                  to={`/addCanal/`}
-                  className="btn btn-primary"
-                >
-                   Dodaj nowy Kanał
-                </Link>
-            </td>
-          </tr>
-        </thead>
-        <tbody>
+        <div className="container mt-5">
+          <div className="row justify-content-md-center">
+            <div className="col-4 grid">
+              <b>Kanał</b>
+            </div>
+            <div className="col-2 grid">
+              <b>Ilość</b>
+            </div>
+            <Link to={`/addCanal/`} className="btn btn-success col-2  col-lg-1">
+              Dodaj
+            </Link>
+            <div className="col-2  col-lg-1"></div>
+          </div>
           {state.canals &&
             state.canals.map((canal) => (
-              <tr>
-                <td className='col-6'>{canal.name}</td>
-                <td className="col-2">{canal.number}</td>
-                <td>
+              <div className="row justify-content-md-center">
+                <div className="col-4 grid">{canal.name}</div>
+                <div className="col-2 grid">{canal.number}</div>
                 <Link
                   to={`/updateCanal/${canal.id}`}
-                  
-                  className="btn btn-primary"
+                  className="btn btn-primary col-2  col-lg-1"
                 >
                   Edytuj
                 </Link>
-                <button className="btn btn-danger" onClick={() => deleteCanal(canal.id)}>Usuń</button>
-                </td>
-                </tr>
-
+                <button
+                  className="btn btn-danger col-2  col-lg-1"
+                  onClick={() => deleteCanal(canal.id)}
+                >
+                  Usuń
+                </button>
+              </div>
             ))}
-        </tbody>
-      </table>
+        </div>
       ) : (
-        <h4 className="text-center">No Canals</h4>
+        <h1>Nie ma</h1>
       )}
     </div>
   );
